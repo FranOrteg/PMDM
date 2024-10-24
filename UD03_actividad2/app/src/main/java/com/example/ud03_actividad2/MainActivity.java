5ffcc63374072474dbb7080c180d9adf690a5255
profesor <nataliaprofedeinformatica@gmail.com>
2024-10-24 16:28:47 +0200

    ut03 actividad2

--- UD03_actividad2/app/src/main/java/com/example/ud03_actividad2/MainActivity.java
+++ UD03_actividad2/app/src/main/java/com/example/ud03_actividad2/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.ud03_actividad2;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etOperando1, etOperando2;
+    TextView tvResultado;
+    Button btnSumar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etOperando1 = (EditText) findViewById(R.id.etOperando1);
+        etOperando2 = (EditText) findViewById(R.id.etOperando2);
+        tvResultado = (TextView) findViewById(R.id.tvResultado);
+        btnSumar = (Button) findViewById(R.id.btnSumar);
+
+        btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1, num2;
+
+                try{
+                    num1=Integer.parseInt(etOperando1.getText().toString());
+                    num2=Integer.parseInt(etOperando2.getText().toString());
+                    tvResultado.setText(""+(num1+num2));
+                }catch(NumberFormatException e){
+                    tvResultado.setText("Los números deben ser enteros");
+                }
+            }
         });
     }
 }
